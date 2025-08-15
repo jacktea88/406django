315dee494412bf01766cd1ca0f17e28a95ac6112
jack <jacktea@gmail.com>
2025-08-15 11:27:47 +0800

    建立產品資料表練習查詢

--- ch02/mblog/mysite/admin.py
+++ ch02/mblog/mysite/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from mysite.models import Post
+from mysite.models import Post, NewTable, Product
 
 # Register your models here.
 # admin.site.register(Post)
+admin.site.register(NewTable)
+admin.site.register(Product)
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'pub_date')
