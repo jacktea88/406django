b33bd2fc6f2ed374003bb986da5d8bdd775b9512
jack <jacktea@gmail.com>
2025-09-09 11:22:37 +0800

    加入評論列表、新增、刪除評論，分類查詢

--- ch05/bstore/api/urls.py
+++ ch05/bstore/api/urls.py
@@ -9,4 +9,18 @@ urlpatterns = [
     # GET http://127.0.0.1:8000/api/books/?category=1 - 取得分類為1的書籍
     # POST http://127.0.0.1:8000/api/books/ - 新增書籍
     path('books/', views.books_list, name='books_list'),
+    # GET http://127.0.0.1:8000/api/books/1/ - 取得特定書籍
+    # PUT http://127.0.0.1:8000/api/books/1/ - 更新書籍  
+    # DELETE http://127.0.0.1:8000/api/books/1/ - 刪除書籍
+    path('books/<int:book_id>/', views.book_detail, name='book_detail'),
+    # GET http://127.0.0.1:8000/api/books/1/reviews/ - 取得書籍評論
+    # POST http://127.0.0.1:8000/api/books/1/reviews/ - 新增評論
+    # DELETE http://127.0.0.1:8000/api/books/1/reviews/ - 刪除評論
+    path('books/<int:book_id>/reviews/', views.book_reviews, name='book_reviews'),
+    # GET http://127.0.0.1:8000/api/categories/ - 取得所有分類
+    path('categories/', views.categories_list, name='categories_list'),
+    # GET http://127.0.0.1:8000/api/categories/1/ - 取得特定分類
+    path('categories/<int:category_id>/', views.category_detail, name='category_detail'),
+
+    
 ]
--- ch05/bstore/api/views.py
+++ ch05/bstore/api/views.py
@@ -23,6 +23,24 @@ REVIEWS_DATA = [
     {'id': 3, 'book_id': 2, 'rating': 5, 'comment': 'Django入門首選', 'user': '讀者C'},
 ]
 
+# ===== 共用的輔助函數 =====
+def fine_book_by_id(book_id):
+    for book in BOOKS_DATA:
+        if book['id'] == book_id:
+            return book
+    return None
+
+def get_reviews_by_book_id(book_id):
+    for review in REVIEWS_DATA:
+        if review['book_id'] == book_id:
+            return review
+    return None
+
+def  find_category_by_id(category_id):
+    for category in CATEGORIES_DATA:
+        if category['id'] == category_id:
+            return category
+    return None
 
 def index_api(request):
     return JsonResponse({
@@ -83,3 +101,108 @@ def books_list(request):
 
 
     return JsonResponse({"books": books})
+
+@csrf_exempt
+def book_detail(request, book_id):
+    """單本書籍詳細資訊端點
+    # GET http://127.0.0.1:8000/api/books/1/ - 取得特定書籍
+    # PUT http://127.0.0.1:8000/api/books/1/ - 更新書籍  
+    # DELETE http://127.0.0.1:8000/api/books/1/ - 刪除書籍
+    """
+    book = fine_book_by_id(book_id)
+    print('book:', book)
+    if not book:
+        return JsonResponse({'error': '書籍不存在'}, status=404)
+    
+    if request.method == 'GET':
+        return JsonResponse({'book': book})
+    
+    elif request.method == 'PUT':
+        try:
+            data = json.loads(request.body)
+            book.update(data)
+            return JsonResponse({
+                'message': '書籍更新成功',
+                'book': book
+            })
+        except json.JSONDecodeError:
+            return JsonResponse({
+                'message': '請提供正確的JSON格式資料'
+            }, status=400)
+    
+    elif request.method == 'DELETE':
+        BOOKS_DATA.remove(book)
+        return JsonResponse({
+            'message': '書籍刪除成功'
+        })
+    
+@csrf_exempt
+def book_reviews(request, book_id):
+    """書籍評論端點
+    GET http://127.0.0.1:8000/api/books/1/reviews/ - 取得特定書籍的評論
+    POST http://127.0.0.1:8000/api/books/1/reviews/ - 新增特定書籍的評論
+    DELETE http://127.0.0.1:8000/api/books/1/reviews/ - 刪除特定書籍的評論
+    """
+    book = fine_book_by_id(book_id)
+    if not book:
+            return JsonResponse({'error': '書籍不存在'}, status=404)
+        
+    if request.method == 'GET':
+        reviews = get_reviews_by_book_id(book_id)
+        return JsonResponse({
+            'book_title': book['title'],
+            'reviews': reviews
+        })
+    
+    elif request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            new_review = {
+                'id': len(REVIEWS_DATA) + 1,
+                'book_id': book_id,
+                'rating': data['rating'],
+                'comment': data['comment'],
+                'user': data['user']
+            }
+            REVIEWS_DATA.append(new_review)
+            return JsonResponse({
+                'message': '評論新增成功',
+                'review': new_review
+            })
+        except json.JSONDecodeError:
+            return JsonResponse({
+                'message': '請提供正確的JSON格式資料'
+            }, status=400)
+    elif request.method == 'DELETE':
+        reviews = get_reviews_by_book_id(book_id)
+        print('REVIEWS_DATA:', REVIEWS_DATA)
+        REVIEWS_DATA.remove(reviews)
+        return JsonResponse({
+            'message': '評論刪除成功'
+        })
+    else:
+        return JsonResponse({'error': '不支援的請求方法'}, status=405)
+    
+
+def categories_list(request):
+    """分類列表端點
+    http://localhost:8000/api/categories/
+    """
+    return JsonResponse({'categories': CATEGORIES_DATA})
+
+def category_detail(request, category_id):
+    """特定分類書籍端點
+    http://localhost:8000/api/categories/1/
+    """
+    category = find_category_by_id(category_id)
+    if not category:
+        return JsonResponse({'error': 'Category not found'}, status=404)
+    
+    # 取得該分類的書籍
+    books = [book for book in BOOKS_DATA if book['category_id'] == category_id]
+    
+    return JsonResponse({
+        'category': category,
+        'books_count': len(books),
+        'books': books
+    })
