98c3034dad17b01f41a87c43d347bfbde12ea357
jack <jacktea@gmail.com>
2025-09-09 14:24:53 +0800

    把全部index.html拆開成index, base, header, footer的組合

--- ch06/templates/mysite/views.py
+++ ch06/templates/mysite/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    msg = 'hello world by render function'
+    return render(request, 'index.html', {'msg': msg})
