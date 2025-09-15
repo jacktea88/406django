0c092e757bd671c292a6bc9920cd6d2942e3a2fa
jack <jacktea@gmail.com>
2025-09-15 16:30:48 +0800

    顯示圖片

--- ch07/dborm/dborm/urls.py
+++ ch07/dborm/dborm/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mysite import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('detail/<int:id>', views.detail, name='detail-url'),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ch07/dborm/mysite/views.py
+++ ch07/dborm/mysite/views.py
@@ -10,6 +10,7 @@ def detail(request, id):
     try:
         product = models.Product.objects.get(id=id)
         images = models.PPhoto.objects.filter(product=product)
+        print('images:',images)
     except:
         pass
     return render(request, 'detail.html', locals())
