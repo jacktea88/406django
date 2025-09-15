6b33ad229e8ec75c41d0919726f1eab58f24e507
jack <jacktea@gmail.com>
2025-09-15 15:53:54 +0800

    顯示產品細節資料

--- ch07/dborm/dborm/urls.py
+++ ch07/dborm/dborm/urls.py
@@ -21,4 +21,5 @@ from mysite import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('detail/<int:id>', views.detail, name='detail-url'),
 ]
