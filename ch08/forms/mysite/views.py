20364482a24e4ca96b3c36e3105f230ba576dd86
jack <jacktea@gmail.com>
2025-09-16 10:29:22 +0800

    基本表單功能驗證

--- ch08/forms/mysite/views.py
+++ ch08/forms/mysite/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    years = range(1912, 2020)
+    try:
+        user_id = request.GET['user_id']
+        user_pass = request.GET['user_pass']
+        user_byear = request.GET['byear']
+        urfcolor = request.GET.getlist('fcolor')
+    except:
+        user_id = None
+    if user_id == 'admin' and user_pass == '12345678':
+        verified = True
+    else:
+        verified = False
     return render(request, 'index.html', locals())
