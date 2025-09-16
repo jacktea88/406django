b04d0e256d8af0d3b65d28b1f6a135a8954e6e13
jack <jacktea@gmail.com>
2025-09-16 11:41:13 +0800

    將留言存入資料庫並顯示

--- ch08/forms/mysite/admin.py
+++ ch08/forms/mysite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from mysite import models
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['nickname', 'message', 'pub_time', 'enabled']
+
+admin.site.register(models.Mood)
+admin.site.register(models.Post, PostAdmin)
--- ch08/forms/mysite/views.py
+++ ch08/forms/mysite/views.py
@@ -1,17 +1,69 @@
 from django.shortcuts import render
+from mysite import models
 
 # Create your views here.
+# def index(request):
+#     years = range(1912, 2020)
+#     try:
+#         user_id = request.GET['user_id']
+#         user_pass = request.GET['user_pass']
+#         user_byear = request.GET['byear']
+#         urfcolor = request.GET.getlist('fcolor')
+#     except:
+#         user_id = None
+#     if user_id == 'admin' and user_pass == '12345678':
+#         verified = True
+#     else:
+#         verified = False
+#     return render(request, 'index.html', locals())
+
+# def index(request):
+#     posts = models.Post.objects.all().order_by('-pub_time')
+#     moods = models.Mood.objects.all()
+#     try:
+#         user_id = request.GET['user_id']
+#         user_pass = request.GET['user_pass']
+#         user_post = request.GET['user_post']
+#         user_mood = request.GET['mood']
+#     except:
+#         # user_id = None
+#         message = '每一欄都要填寫'
+    
+#     # if user_id != None:
+#         mood = models.Mood.objects.get(status = user_mood)
+#         post = models.Post(mood = mood, nickname = user_id, message = user_post, del_pass = user_pass)
+#         # post = models.Post( ,nickname = user_id, message = user_post, del_pass = user_pass)
+#         # post.save()
+#         message = '張貼成功'
+#     # else:
+#         message = '無效的帳號'
+
+#     return render(request, 'index_post.html', locals())
+
 def index(request):
-    years = range(1912, 2020)
+    posts = models.Post.objects.all().order_by('-pub_time')
+    moods = models.Mood.objects.all()
     try:
+        # user_id = request.POST['user_id']
+        # user_pass = request.POST['user_pass']
+        # user_post = request.POST['user_post']
+        # user_mood = request.POST['mood']
         user_id = request.GET['user_id']
         user_pass = request.GET['user_pass']
-        user_byear = request.GET['byear']
-        urfcolor = request.GET.getlist('fcolor')
+        user_post = request.GET['user_post']
+        user_mood = request.GET['mood']
+
+        print(user_id, user_pass, user_post, user_mood)
     except:
         user_id = None
-    if user_id == 'admin' and user_pass == '12345678':
-        verified = True
+        message = '每一欄都要填寫'
+    if user_id != None:
+        
+        mood = models.Mood.objects.get(status = user_mood)
+        post = models.Post(mood = mood, nickname = user_id, message = user_post, del_pass = user_pass)
+        post.save()
+        message = '張貼成功'
     else:
-        verified = False
-    return render(request, 'index.html', locals())
+        message = '每一欄都要填寫'
+    # print(user_id, user_pass, user_post, user_mood)    
+    return render(request, 'index_post.html', locals())
