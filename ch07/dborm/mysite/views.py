5e72a46bb79cfb787b43b18d7c8365d632abfe3d
jack <jacktea@gmail.com>
2025-09-16 08:57:33 +0800

    關聯查詢補充

--- ch07/dborm/mysite/views.py
+++ ch07/dborm/mysite/views.py
@@ -3,13 +3,13 @@ from mysite import models
 
 # Create your views here.
 def index(request):
-    products = models.Product.objects.all()
+    products = models.Product.objects.all() #多對一的查詢
     return render(request, 'index.html', locals())
 
 def detail(request, id):
     try:
         product = models.Product.objects.get(id=id)
-        images = models.PPhoto.objects.filter(product=product)
+        images = models.PPhoto.objects.filter(product=product)  #一對多的查詢
         print('images:',images)
     except:
         pass
