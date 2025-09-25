14f4a4879b0d5db80a563e937750477c35d51466
jack <jacktea@gmail.com>
2025-09-25 09:56:05 +0800

    測試cookie功能是否可用

--- ch09/chart/mysite/views.py
+++ ch09/chart/mysite/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.session.test_cookie_worked():
+        request.session.delete_test_cookie()
+        message = 'Cookie 已成功啟用'
+    else:
+        message = 'Cookie 啟用失敗'
+        request.session.set_test_cookie()
     return render(request, 'index.html', locals())
