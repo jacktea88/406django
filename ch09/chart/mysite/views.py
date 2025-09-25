ed058c69d10ada461af6be86d7f0822a5261fefc
jack <jacktea@gmail.com>
2025-09-25 13:34:12 +0800

    改用django auth功能來登入

--- ch09/chart/mysite/views.py
+++ ch09/chart/mysite/views.py
@@ -3,6 +3,11 @@ from mysite import forms, models
 from django.contrib.sessions.models import Session
 from django.contrib import messages
 
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 # def index(request):
 #     if request.session.test_cookie_worked():
@@ -14,9 +19,23 @@ from django.contrib import messages
 #     return render(request, 'index.html', locals())
 
 # 檢查username是否存在
+# def index(request):
+#     if 'username' in request.session:
+#         username = request.session['username']
+#         print('username:in index', username)
+#         # message = '登入成功'
+#         messages.info(request, '登入成功')
+#     else:
+#         username = None
+#         print('username不存在')
+#         # message = '登入失敗'
+#         messages.warning(request, '登入失敗')
+#     return render(request, 'index.html', locals())
+
+# use django auth for index
 def index(request):
-    if 'username' in request.session:
-        username = request.session['username']
+    if request.user.is_authenticated:
+        username = request.user.username
         print('username:in index', username)
         # message = '登入成功'
         messages.info(request, '登入成功')
@@ -24,30 +43,54 @@ def index(request):
         username = None
         print('username不存在')
         # message = '登入失敗'
-        messages.warning(request, '登入失敗')
+        messages.warning(request, 'USER不存在，登入失敗')
     return render(request, 'index.html', locals())
 
+
+# def login(request):
+#     if request.method == 'POST':
+#         login_form = forms.LoginForm(request.POST)
+#         if login_form.is_valid():
+#             login_name=request.POST['username'].strip()
+#             login_password=request.POST['password']
+#             print('login_name:', login_name)
+#             try:
+#                 user = models.User.objects.get(name=login_name)
+#                 if user.password == login_password:
+#                     request.session['username'] = login_name
+#                     messages.warning(request, '成功登入了')
+#                     return redirect('/')
+#                 else:
+#                     # message = '密碼錯誤'
+#                     messages.warning(request, '密碼錯誤')
+#             except:
+#                 # message = '找不到使用者'
+#                 messages.warning(request, '找不到使用者')
+#         else:
+#             # message = '請檢查輸入的欄位內容'
+#             messages.warning(request, '請檢查輸入的欄位內容')
+#     else:   # GET
+#         login_form = forms.LoginForm()
+#     return render(request, 'login.html', locals())
+
+# use django auth login
 def login(request):
     if request.method == 'POST':
         login_form = forms.LoginForm(request.POST)
         if login_form.is_valid():
             login_name=request.POST['username'].strip()
             login_password=request.POST['password']
-            print('login_name:', login_name)
-            try:
-                user = models.User.objects.get(name=login_name)
-                if user.password == login_password:
-                    request.session['username'] = login_name
+            user = authenticate(username=login_name, password=login_password)
+            if user is not None:
+                if user.is_active:
+                    auth.login(request, user)
                     messages.warning(request, '成功登入了')
                     return redirect('/')
                 else:
-                    # message = '密碼錯誤'
-                    messages.warning(request, '密碼錯誤')
-            except:
-                # message = '找不到使用者'
-                messages.warning(request, '找不到使用者')
+                    messages.warning(request, '帳號尚未啟用')
+            else:
+                messages.warning(request, '登入失敗')
         else:
-            # message = '請檢查輸入的欄位內容'
             messages.warning(request, '請檢查輸入的欄位內容')
     else:   # GET
         login_form = forms.LoginForm()
