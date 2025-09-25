bca0f6c8ff77d5d94c38a93cc2695cb0c2a82e24
jack <jacktea@gmail.com>
2025-09-25 15:15:21 +0800

    建立vote table並匯入現成的資料

--- ch09/chart/mysite/models.py
+++ ch09/chart/mysite/models.py
@@ -20,3 +20,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username 
+
+# vote model
+class Vote(models.Model):
+    name = models.CharField(max_length=20)
+    no = models.IntegerField()
+    sex = models.BooleanField(default=False)
+    byear = models.IntegerField()
+    party = models.CharField(max_length=20)
+    votes = models.IntegerField()
+
+    def __str__(self):
+        return self.name
