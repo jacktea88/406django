367e9943e968c8f847e9d6f511b50966d89b3206
jacktea88 <jacktea@gmail.com>
2025-11-10 21:42:09 +0800

    今日名言by random & render

--- ch04/phone/mysite/views.py
+++ ch04/phone/mysite/views.py
@@ -1,21 +1,29 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 def about(request):
-    html = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>about</title>
-</head>
-<body>
-    <h2>Somebody</h2><hr>
-    <p>This is the about page</p>
-</body>
-</html>
-    '''
+#     html = '''
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#     <title>about</title>
+# </head>
+# <body>
+#     <h2>Somebody</h2><hr>
+#     <p>This is the about page</p>
+# </body>
+# </html>
+#     '''
+    quotes = ['今日事，今日畢',
+            '要怎麼收穫，先那麼栽',
+            '知識就是力量',
+            '一個人的個性就是他的命運']
+
+    quote = random.choice(quotes)
+
     # return HttpResponse(html)
-    return render(request, 'about.html')
+    return render(request, 'about.html', locals())
