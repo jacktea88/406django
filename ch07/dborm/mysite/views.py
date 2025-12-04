8112c65892032d21a1c77ffe38204395d77576c2
jacktea88 <jacktea@gmail.com>
2025-12-04 15:45:05 +0800

    簡化html檔，原index.html改成bakok做備份，程式ok可跑

--- ch07/dborm/mysite/views.py
+++ ch07/dborm/mysite/views.py
@@ -4,13 +4,15 @@ from mysite import models
 # Create your views here.
 def index(request):
     products = models.Product.objects.all() #多對一的查詢
+    
     return render(request, 'index.html', locals())
 
 def detail(request, id):
     try:
         product = models.Product.objects.get(id=id)
-        images = models.PPhoto.objects.filter(product=product)  #一對多的查詢
+        images = models.PPhoto.objects.filter(product=product)  #針對此product做一對多的查詢
         print('images:',images)
     except:
         pass
+    
     return render(request, 'detail.html', locals())
