caa9eeb5368b40b5cc3fde4b2b4d7c87749b3ea9
jacktea88 <jacktea@gmail.com>
2026-05-28 23:09:02 +0800

    加入go power網頁，但還沒整合到django

--- ch06/templates/templates/settings.py
+++ ch06/templates/templates/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'templates.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        # 'DIRS': [BASE_DIR / 'templates'],
+        # 'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
